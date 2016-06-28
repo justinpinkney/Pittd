ad95fab25e83aa3b0876eb60219071fd46753ebf
Justin <justinpinkney@gmail.com>
2016-06-28 09:50:03 +0100

    Parser update route

--- pittd/viewer/viewer.py
+++ pittd/viewer/viewer.py
@@ -14,6 +14,10 @@ def download_file(filename):
     return send_from_directory(RECORD_DIRECTORY,
                                filename, as_attachment=True)
 
+@app.route('/update')
+def update():
+    parser.update()
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
